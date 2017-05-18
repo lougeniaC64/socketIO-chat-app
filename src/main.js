b77d90432c62a742ecaabc04e5206723471876a1
lougeniaC64 <lougenia.bailey@gmail.com>
2017-05-18 17:07:33 -0400

    Removed semicolons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Messages from './Messages';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Messages from './Messages'
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
