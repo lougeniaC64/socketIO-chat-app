eedc1e906efbcd537faa88094d916b8489306c83
lougeniaC64 <lougenia.bailey@gmail.com>
2017-05-18 17:09:05 -0400

    Converted code to ES6

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Messages from './Messages'
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () =>
   ReactDOM.render(
     React.createElement(Messages),
     document.getElementById('mount')
-  );
-});
+  )
+)
